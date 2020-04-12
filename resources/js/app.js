9e063b0b3a17a8e33c7421970ba66865b78d90d1
Darjus Kazakevic <43537114+darjusk@users.noreply.github.com>
2020-04-12 11:18:57 +0300

    init leaflet vue project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,13 @@
 window.Vue = require('vue');
 
 import Harbour from './components/Harbour';
+import { LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+
 Vue.component('harbour', Harbour);
 window.app = new Vue(
     {
--- resources/js/components/Harbour.vue
+++ resources/js/components/Harbour.vue
@@ -1,49 +1,100 @@
 <template>
-    <GoogleMapLoader
-            :mapConfig="mapConfig"
-            apiKey="AIzaSyAmHh2gluvEpciOs4SPJDn1j-AqeVi5v8I"
-    >
-        <template slot-scope="{ google, map }">
-            <GoogleMapMarker
-                    v-for="marker in markers"
-                    :key="marker.id"
-                    :marker="marker"
-                    :google="google"
-                    :map="map"
+    <div style="height: 500px; width: 100%">
+        <l-map
+                v-if="showMap"
+                :zoom="zoom"
+                :center="center"
+                :options="mapOptions"
+                style="height: 80%"
+                @update:center="centerUpdate"
+                @update:zoom="zoomUpdate"
+        >
+            <l-tile-layer
+                    :url="url"
+                    :attribution="attribution"
             />
-        </template>
-    </GoogleMapLoader>
+            <div v-for="marker in markers">
+                <l-marker :lat-lng="marker" :icon="icon">
+                    <l-popup>
+                        <div>
+                            Weather
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                                sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+                                Donec finibus semper metus id malesuada.
+                            </p>
+                        </div>
+                    </l-popup>
+                </l-marker>
+            </div>
+        </l-map>
+    </div>
 </template>
 
 <script>
-    import GoogleMapLoader from "./GoogleMapLoader";
-    import GoogleMapMarker from "./GoogleMapMarker";
-
+    import { latLng, icon } from "leaflet";
+    import { LMap, LTileLayer, LMarker, LPopup, LTooltip} from "vue2-leaflet";
+    import axios from "axios"
     export default {
+        name: "Example",
         components: {
-            GoogleMapLoader,
-            GoogleMapMarker,
+            LMap,
+            LTileLayer,
+            LMarker,
+            LPopup,
+            LTooltip
         },
-        data: function () {
+        data() {
             return {
-                markers: [
-                    { id: 'a', position: { lat: 3, lng: 101 } },
-                    { id: 'b', position: { lat: 5, lng: 99 } },
-                    { id: 'c', position: { lat: 6, lng: 97 } },
-                ],
-            }
+                zoom: 2,
+                center: latLng(47.41322, -1.219482),
+                url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attribution:
+                    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                currentZoom: 11.5,
+                currentCenter: latLng(14.5617, 121.0214),
+                showParagraph: false,
+                mapOptions: {
+                    zoomSnap: 0.5
+                },
+                showMap: true,
+                markers: [],
+                icon: icon({
+                    iconUrl:'pngfuel.com.png',
+                    iconSize: [20, 20],
+                    iconAnchor: [16, 37]
+                }),
+            };
         },
-
-        computed: {
-            mapConfig () {
-                return {
-                    center: this.mapCenter
-                }
+        methods: {
+            zoomUpdate(zoom) {
+                this.currentZoom = zoom;
             },
-
-            mapCenter () {
-                return this.markers[1].position
+            centerUpdate(center) {
+                this.currentCenter = center;
+            },
+            getHarbours() {
+                axios
+                    .get('https://devapi.harba.co/harbors/visible')
+                    .then(response => {
+                        response.data.forEach(harbour =>
+                            this.markers.push(
+                                latLng(harbour.lat, harbour.lon)
+                            )
+                        );
+                        this.currentCenter = this.markers[0]
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        this.errored = true
+                    })
             }
         },
-    }
+        mounted() {
+            this.getHarbours();
+            // this.$nextTick(() => {
+            //     this.$refs.myMap.mapObject.setIcon('pngfuel.com.png');
+            // });
+        },
+    };
 </script>
